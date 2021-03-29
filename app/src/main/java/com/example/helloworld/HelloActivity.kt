d75bd905c219d35f703d3e62ca0b4613965dcf42
DlfMinn <dlfminn675@gmail.com>
2021-03-29 22:31:33 +0300

    Initial commit

---
+++ app/src/main/java/com/example/helloworld/HelloActivity.kt
@@ -0,0 +1,12 @@
+package com.example.helloworld
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HelloActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.hello_activity)
+    }
+}
